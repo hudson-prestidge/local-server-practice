dccec2470ec2a8f49c248fa27ba5c734adf34461
Hudson Prestidge <charcon@ihug.co.nz>
2016-07-24 22:28:08 +1200

    move route into seperate routes file

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 var express = require('express')
-var routes = require(./routes/routes.js)
+var routes = require('./routes/routes.js')
 
 var PORT = 3000
 var app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello World!')
-})
+app.get('/', routes.index)
 
 app.listen(PORT, function() {
   console.log('The server is running, and is listening on port', PORT)
