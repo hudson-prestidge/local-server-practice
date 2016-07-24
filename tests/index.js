ed49e8882db9f82c11a5476da6c77d87c054d79d
Hudson Prestidge <charcon@ihug.co.nz>
2016-07-24 23:03:30 +1200

    wrote test for routes.webApps

--- tests/index.js
+++ tests/index.js
@@ -23,9 +23,29 @@ test('routes.home responds with index.html', function(t) {
   routes.home(null, res)
 
   function sendFile (file) {
-    t.equals(file, "/Users/Charcon/Workspace/local-server-practice/routes/index.html")
+    t.equals(file, "/Users/Charcon/Workspace/local-server-practice/routes/../index.html")
     //this test sucks, because it's hard coded to only work in this particular directory
     //on this particular computer
+    //part two, it now sucks even more because you included a '..' in the path.
+    t.end()
+  }
+})
+
+
+test('routes.webapps response includes a querystring parameter', function(t) {
+  var res = {
+    send: send
+  }
+  var req = {
+    query: {
+      name: 'Hudson'
+    }
+  }
+
+  routes.webApps(req, res)
+
+  function send (msg) {
+    t.equals(msg, "<h1>Hudson is building web apps!</h1>")
     t.end()
   }
 })
